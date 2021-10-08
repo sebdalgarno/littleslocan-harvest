4cb1ab96dee09dd35beb23a7137f9be6ec104480
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-10-07 17:05:16 -0700

    better info

--- src/Components/About.svelte
+++ src/Components/About.svelte
@@ -8,6 +8,10 @@
     @apply text-xl pt-4 pb-1;
   }
 
+  .subtitle {
+    @apply text-base pt-4 pb-1;
+  }
+
   .text {
     @apply text-sm text-gray-900;
   } 
@@ -20,10 +24,11 @@
 </style>
 
 <div class="z-50">
+  <p class="title">Welcome!</p>
   <p class="title">Tips for exploring the data</p>
   <p class="text">{tips}</p>
-  <p class="title">Data</p>
+  <p class="subtitle">Data sources</p>
   <p class="smalltext">{data}</p>
-  <p class="title">Credit</p>
-  <p class="smalltext">Data were compiled by Dave Leversee. The visualization was developed by Seb Dalgarno (<a href="https://www.northbeachconsulting.ca" target="_blank">North Beach Consulting</a>). It is a new implementation of John Broadhead's original animation and much of the original design is mimicked. Contact <a href="mailto:seb@northbeachconsulting.ca">Seb Dalgarno</a> for more information.</p>
+  <p class="subtitle">Credit</p>
+  <p class="smalltext">Data were compiled by Dave Leversee. The visualization was developed by Seb Dalgarno (<a href="https://www.northbeachconsulting.ca" target="_blank">North Beach Consulting</a>). The idea and design borrow heavily from John Broadhead's animation of Haida Gwaii logging history. Contact <a href="mailto:seb@northbeachconsulting.ca">Seb Dalgarno</a> for more information.</p>
 </div>
--- src/about.js
+++ src/about.js
@@ -5,4 +5,4 @@ export const data =
   "VRI (2019), Harvest RESULTS and Reserves (2020), Consolidated Cutblocks (2020), Harvest Authority (2020), NRCAN Landsat Time Series (1985-2015), Sentinel-2 imagery (August 2019, June 2020).";
 
 export const tips =
-  "To move forward and backward in time, either move the slider, click +/- buttons, or click the slider and press the left/right arrow keys on your keyboard. Use the mouse to zoom or pan the map. Hover mouse over chart and logged polygons on map to see more detailed info.";
+  "To move forward and backward in time, either move the slider, click +/- buttons, or click the slider and press the left/right arrow keys on your keyboard. Use the mouse to zoom or pan the map. Hover mouse over chart and logged polygons on map to see more detailed info in popups.";
